d4da5c725098d14f14e19d2846d3b456b6fb3c63
Andrew Rowberry <rowbs.js@gmail.com>
2024-11-09 07:31:54 -0800

    Update index.js

--- index.js
+++ index.js
@@ -55,5 +55,5 @@ app.get("/check-follower", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on PORT:${PORT}`);
 });
